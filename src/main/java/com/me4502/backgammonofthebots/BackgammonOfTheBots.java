279fb8949d23d7833d3d1ac5f46a5d809e9d9cf5
Matthew Miller <mnmiller1@me.com>
2016-11-08 23:28:51 +1000

    Added stub files

---
+++ src/main/java/com/me4502/backgammonofthebots/BackgammonOfTheBots.java
@@ -0,0 +1,9 @@
+package com.me4502.backgammonofthebots;
+
+public class BackgammonOfTheBots {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/me4502/backgammonofthebots/server/WebsocketImpl.java
@@ -0,0 +1,34 @@
+package com.me4502.backgammonofthebots.server;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
+public class WebsocketImpl extends WebSocketServer {
+
+    public WebsocketImpl(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+
+    }
+}
